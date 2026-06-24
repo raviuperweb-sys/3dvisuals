2d98b3411cb475a60d9e6a0fcf6a88bf222de31a
raviuperweb-sys <ravi.uperweb@gmail.com>
2026-06-24 12:20:48 +0530

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -7,10 +7,9 @@ const adminSchema = new mongoose.Schema({
   name:     { type: String, default: 'Admin' },
 }, { timestamps: true });
 
-adminSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+adminSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 adminSchema.methods.comparePassword = function(plain) {
